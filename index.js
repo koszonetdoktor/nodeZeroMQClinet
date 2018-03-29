2055416cb78ac9189371cb355eb27f74c22f7cb1
Gergo <halmi.gergo@gmail.com>
2018-03-29 21:44:16 +0200

    able to subscribe on another port

--- index.js
+++ index.js
@@ -5,26 +5,67 @@
 var zmq = require('zeromq');
 
 // socket to talk to server
-console.log("Connecting to hello world server...");
+
 var requester = zmq.socket('req');
+var subber = zmq.socket("sub")
+console.log("connect to pub")
+subber.connect("tcp://localhost:5556");
+
+subber.subscribe('ge'); // felismerem, de nem értem miért nem tudom szépen kiolvasni akkor :(
+console.log('Subscriber connected to port 5556');
+
+subber.on('message', function(topic, message) {
+  console.log(topic.toString("utf8"));
+});
 
 var x = 0;
+
+console.log("Connecting to the server...");
+requester.connect("tcp://localhost:5555");
+
+console.log("Sending Start request");
+requester.send("start");
+
+let step = 0;
+
 requester.on("message", function(reply) {
-  console.log("Received reply", x, ": [", reply.toString(), ']');
-  x += 1;
-  if (x === 10) {
-    requester.close();
-    process.exit(0);
+  console.log(reply.toString());
+  switch(step) {
+    case 0: // getting answer for the performed subscription
+      evaluateReply(reply);
+      break;
+    case 1: // we are getting the values 
+      console.log("val: ", reply);
+      break;
+    case 2:
+      evaluateReply(reply);
+      break;
   }
 });
 
-requester.connect("tcp://localhost:5555");
-
-for (var i = 0; i < 10; i++) {
-  console.log("Sending request", i, '...');
-  requester.send("Hello");
-}
+setTimeout(function() {
+  requester.send("stopp");
+  console.log("Stop message is sent!")
+  step = 2;
+},10000);
 
 process.on('SIGINT', function() {
   requester.close();
 });
+
+function evaluateReply(reply) {
+  if(reply.includes("ok")) {
+    console.log("Reply was ok");
+    if(step === 2) {
+      console.log("Process is done!");
+      requester.close();
+      process.exit();
+    }
+  } else if(reply === "er") {
+    console.error("error occured!");
+  } else if(reply === "uk") {
+    console.warn("Server didnt understand the message!");
+  } else {
+    console.warn("unknown reply from the server");
+  }
+}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hwllo World";
+    return 0;
+}
